1e92924e4216cab76d8697e2a7dc61d2f4ccbdca
hungryyang <ycr1714336458@163.com>
2017-06-27 20:17:22 +0800

    修改Redux结构

--- src/editor/reducer.js
+++ src/editor/reducer.js
@@ -1,12 +1,14 @@
 import {EDIT_NOW,SAVE_FILE} from './actionTypes.js'
 
 const initialState = {
-    value:''
+    "value":'',
+    "file":[]
 }
 
-
-
-export default (state = initialState,action) => {
+export default (state ,action) => {
+    if (Object.getOwnPropertyNames(state).length === 0) {
+        return initialState    
+    }
     switch(action.type){
         case EDIT_NOW:{
             var newState = Object.assign({},state,{value:action.value})
@@ -14,9 +16,11 @@ export default (state = initialState,action) => {
         }
 
         case SAVE_FILE:{
-            var obj = new Object()
+            var newFile = state.file.slice()
+            var obj = {}
             obj[`${action.fileName}`]={fileName:action.fileName,value:state.value}
-            newState = Object.assign({},state,obj)
+            newFile.push(obj)
+            newState = Object.assign({},state,{file:newFile})
             return newState
         }
 
