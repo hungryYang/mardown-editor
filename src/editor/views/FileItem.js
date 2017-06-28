b41aa0c4de5eb6f8e5ff24221c92f54563822107
hungryyang <ycr1714336458@163.com>
2017-06-28 15:32:55 +0800

    添加下载功能

--- src/editor/views/FileItem.js
+++ src/editor/views/FileItem.js
@@ -3,11 +3,13 @@ import {connect} from 'react-redux'
 import {Button} from 'antd'
 import {editNow,deleteFile} from '../actions.js'
 const FileItem = ({fileName,fileValue,onEdit,onDel})=>{
+    var blob = new Blob([fileValue])
     return (
         <li>  
             <div onClick={()=>{onEdit(fileValue)}}>
                 {fileName}
                 <Button type="danger" onClick={()=>{onDel(fileName)}}>Delete</Button>
+                <a href={window.URL.createObjectURL(blob)} download={fileName+'.md'}>下载</a>
             </div>  
         </li>         
     )
