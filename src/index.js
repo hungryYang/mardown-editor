8e79e7648759438051e4a1a3ba90d8de2c016d03
hungryyang <ycr1714336458@163.com>
2017-07-02 20:50:18 +0800

    deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import 'antd/dist/antd.css'
 
 import store from './Store.js'
 import MarkdownEditor from './markdownEditor.js'
 
 import './style.scss'
-import './editor/css/style.scss'
 ReactDOM.render(
     <Provider store={store}>
         <MarkdownEditor/>
